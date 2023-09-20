3e606daa68bafc3fa8140bd667478331d24df27f
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-09-20 17:15:21 +0900

    src/lib.rsにsubscribe()の実装を追加してPOST: /subscriptionsが200を返すように

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,15 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse{
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
