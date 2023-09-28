1cb9368c1eb9602fecbc995fdd488ddacd9b4fcf
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-09-28 17:15:17 +0900

    feat: DB接続周りの設定をして結合テストを書き直した。
    PgConnectionだと同時に1つしか接続できないからPgPoolに直したけど、じゃあPgConnectionの存在意義は何？APIがパラメータを受け取れるようになった。\sqlxはSQLそのまんまを書く必要があるな。ORMとは違うっぽい
    rustのRaw String見たいのあったんだ。

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,22 @@
-use std::net::TcpListener;
-use actix_web::{web, App, HttpServer};
-use actix_web::dev::Server;
 use crate::routes::{health_check, subscribe};
-pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| {
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+) -> std::io::Result<Server> {
+    // Wrap the connection in a smart pointer
+    let db_pool = web::Data::new(db_pool);
+    // Capture 'connection' from the surrounding envirnment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the default route, "/" with a GET method
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
