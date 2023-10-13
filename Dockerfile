5b15a2b0f6123357d4059c2c52240289c72d9c46
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-10-13 19:03:15 +0900

    feat: Dockerファイルを書き換えてイメージサイズを減少させる

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM rust:1.73.0
+#build stage
+FROM rust:1.73.0 AS builder
 
 WORKDIR /app
 # Install the required system dependacies for our linking configuration
@@ -6,5 +7,17 @@ RUN apt update && apt install lld clang -y
 COPY . .
 ENV SQLX_OFFLINE true
 RUN cargo build --release
+
+# Runtime stage
+FROM debian:bullseye-slim AS runtime
+WORKDIR /app
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends openssl ca-certificates \
+    # clean up
+    && apt-get autoremove -y \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /app/target/release/zero2prod zero2prod
+COPY configuration configuration
 ENV APP_ENVIRONMENT production
-ENTRYPOINT [ "./target/release/zero2prod" ]
+ENTRYPOINT [ "./zero2prod" ]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
 
@@ -13,7 +13,9 @@ async fn main() -> std::io::Result<()>{
 
     // Panic if we can't read the configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect Postgres.");
     // We have removed the hard-cored `8000` - it's now coming from our settings!
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
