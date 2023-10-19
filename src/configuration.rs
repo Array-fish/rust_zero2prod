425a15d704647eae26b667a1d8cefbb8c149f9a8
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-10-19 16:42:40 +0900

    feat: 環境変数でパラメータを変更できるように変更　データベース設定wo オブジェクトで渡すように

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,7 @@
 use secrecy::{Secret, ExposeSecret};
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
+
 #[derive(serde::Deserialize)]
 pub struct Settings{
     pub database: DatabaseSettings,
@@ -7,6 +10,7 @@ pub struct Settings{
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings{
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -15,9 +19,24 @@ pub struct ApplicationSettings{
 pub struct DatabaseSettings{
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+impl DatabaseSettings{
+    
+    pub fn without_db(&self) -> PgConnectOptions{
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+    }
+    pub fn with_db(&self) -> PgConnectOptions{
+        self.without_db().database(&self.database_name)
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError>{
@@ -34,6 +53,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError>{
     let settings = config::Config::builder()
         .add_source(config::File::from(configuration_directory.join("base.yaml")))
         .add_source(config::File::from(configuration_directory.join(&environment_filename)))
+        .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
         .build()?;
 
     // Try to convert the configuration values it read into out Setting type
@@ -70,18 +90,3 @@ impl TryFrom<String> for Environment {
     }
 }
 
-impl DatabaseSettings{
-    pub fn connection_string(&self) -> Secret<String>{
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password.expose_secret(), self.host, self.port, self.database_name
-        ))
-    }
-    
-    pub fn connection_string_without_db(&self) -> Secret<String>{
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username, self.password.expose_secret(), self.host, self.port
-        ))
-    }
-}
