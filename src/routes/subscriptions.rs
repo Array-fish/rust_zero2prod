81fd8b7b078edd8b77f0123bf3765dea39daa9f1
Array-fish <uekusa_hideaki@yahoo.co.jp>
2023-11-15 15:48:28 +0900

    feat: SubscriberName構造体のparse()がResult型でエラーを返す。単体テストを追加。subscribe APIがnameが不正の場合に400を返すようになった.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
-use unicode_segmentation::UnicodeSegmentation;
 use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
@@ -23,9 +22,13 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse{
+    let name = match SubscriberName::parse(form.0.name){
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let new_subscriber = NewSubscriber{
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+        name,
     };
     match insert_subscriber(&pool,&new_subscriber).await {
         Ok(_) => {
